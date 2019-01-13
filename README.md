acce7e183842b9d1f0a0180df4c8d86bca72d7f6
Juane Garcia <juane_619@hotmail.com>
2019-01-13 12:01:38 +0100

    Project re uploaded

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # android-app-museum
 Android app about biodiversity museum
+
+Pretty interface.
+Easy touching interaction and *voice interaction*.
+Use of DialogFlow API. 
+Use of lib to manage QR codes.
+Use of lib to manage NFC.
+Manage requests POST to interactions with voice using Wikipedia API.
+
---
+++ app/src/main/java/adrijuanejulio/com/biodomointeractivo/TarifasActivity.java
@@ -0,0 +1,14 @@
+package adrijuanejulio.com.biodomointeractivo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Activity tarifas, en ella saldrán las tarifas y precios del Biodomo, es una actividad llena de textView
+
+public class TarifasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarifas);
+    }
+}
